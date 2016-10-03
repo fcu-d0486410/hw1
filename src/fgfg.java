cc661324443802365129360be1b3e26decde06fe
PC235 <PC235@PC235-1B>
2016-10-03 16:53:11 +0800

    Homework1

---
+++ src/fgfg.java
@@ -0,0 +1,12 @@
+
+public class fgfg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String lyrics="Let it go! Let it go! Cannot hold it back anymore";
+				lyrics=lyrics.replace("it", "\"her\"");
+				lyrics=lyrics.replaceAll("Cannot", "Can't");
+		System.out.println(lyrics);
+	}
+
+}
